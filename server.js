67bc922494fc4891ed63534ffdfd8c794176722f
Daniel Nunes <danyribeironunes15@gmail.com>
2024-12-05 19:02:51 +0000

    feat: Add missing milestones and calendar endpoints

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import axios from "axios";
 import FormData from "form-data";
-// import jwt from "jsonwebtoken";
 import { states, createUser, getUser, ghStates, pdp, setUserGhToken, ghLogout } from "./data.js";
 import "dotenv/config";
 
@@ -15,6 +14,7 @@ const GITHUB_CALLBACK = "github/callback";
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const verifyAccess = async (req, res, next) => {
@@ -39,7 +39,7 @@ const verifyAccess = async (req, res, next) => {
     res.redirect("/");
 }
 
-app.get("/", (_, res) => res.send("<a href='/login'>Use Google Account</a>"));
+app.get("/", (_, res) => res.send(`<a href="/login">Use Google Account</a>`));
 
 app.get("/login", (_, res) => {
     const authUrl = new URL("https://accounts.google.com/o/oauth2/v2/auth");
@@ -73,8 +73,6 @@ app.get(`/${LOGIN_CALLBACK}`, async (req, res) => {
 
         try {
             const gAuth = await axios.post("https://www.googleapis.com/oauth2/v3/token", form, { headers: form.getHeaders() });
-            // const jwtPayload = jwt.decode(gAuth.data.id_token);
-
             const userInfo = await axios.get("https://openidconnect.googleapis.com/v1/userinfo", {
                 headers: {
                     Authorization: `Bearer ${gAuth.data.access_token}`
@@ -97,9 +95,9 @@ app.get("/dashboard", verifyAccess, async (req, res) => {
     const { user } = req;
 
     res.send(`
-        <div style="margin-bottom: 2rem; display: flex;">
+        <div style="margin-bottom: 2rem; display: flex; align-items: center;">
             <img src="${user.picture}" style="margin-right: 1rem; border-radius: 1rem;" />
-            <div>
+            <div style="height: fit-content;">
                 <p style="margin: 0 0 0.5rem 0;">Hello <b>${user.name} (${user.email})</b></p>
                 <p style="margin: 0;">Your role is: <b>${user.role}</b></p>
             </div>
@@ -110,6 +108,9 @@ app.get("/dashboard", verifyAccess, async (req, res) => {
                 <a href="/github" style="margin-left: 0.5rem;">Login with GitHub</a>
             ` : ""}
             <input type="submit" style="display: block; margin-top: 0.5rem;" />
+            ${req.query.notfound ? `
+                <p style="margin: 0.5rem 0 0 0;">Repository not found. If the repository is private, make sure you're logged in!</p>
+            ` : ""}
         </form>
         <a href="/logout" style="display: block; margin-top: 3rem;">Logout</a>
         ${user.ghToken ? `
@@ -159,6 +160,90 @@ app.get(`/${GITHUB_CALLBACK}`, verifyAccess, async (req, res) => {
         }
     }
 });
+
+app.get("/milestones", verifyAccess, async (req, res) => {
+    const repo = req.query.repo;
+    if (!repo) {
+        res.send("Missing repo");
+        return;
+    }
+
+    const linkPieces = /^https?:\/\/github\.com\/([^/]+)\/([^/]+)/.exec(repo);
+    if (!linkPieces)
+        res.redirect("/dashboard");
+    else {
+        try {
+            const ghMilestones = await axios.get(`https://api.github.com/repos/${linkPieces[1]}/${linkPieces[2]}/milestones`, { headers: { "Accept": "application/vnd.github+json", "Authorization": req.user.ghToken ? `Bearer ${req.user.ghToken}` : undefined } });
+            const dateFormatter = new Intl.DateTimeFormat("en-US", { dateStyle: "long", timeStyle: "short" });
+
+            res.send(`
+                <h1>Milestones for ${linkPieces[1]}/${linkPieces[2]}</h1>
+                ${ghMilestones.data.map(m => `
+                    <div style="margin-bottom: 3rem;">
+                        <form method="POST" action="../calendar">
+                            <h2 style="margin: 0 0 0.5rem 0;">${m.title}</h2>
+                            <p style="margin: 0 0 0.5rem 0;">${m.description}</p>
+                            <p style="margin: 0;"><b>Due on:</b> ${m.due_on != null ? dateFormatter.format(new Date(m.due_on)) : "No due date"}</p>
+                            ${req.user.role != "free" && m.due_on != null ? `
+                                <input type="hidden" name="name" value="${m.title}" />
+                                <input type="hidden" name="description" value="${m.description}" />
+                                <input type="hidden" name="date" value="${m.due_on}" />
+                                <input type="hidden" name="repo" value="${repo}" />
+                                <input type="submit" value="Add to calendar" style="margin-top: 1rem;" />
+                            ` : ""}
+                        </form>
+                    </div>
+                `).join("")}
+                ${req.query.success ? `
+                    <script>alert("${req.query.success == "true" ? "Milestone added to your Google Calendar!" : "There was an error adding the milestone to your Google Calendar."}");</script>
+                `: ""}
+            `);
+        }
+        catch (error) {
+            console.log(error);
+            res.redirect("/dashboard?notfound=true");
+        }
+    }
+});
+
+app.post("/calendar", verifyAccess, async (req, res) => {
+    const { name, description, date, repo } = req.body;
+
+    if (name === undefined || description === undefined || date === undefined || repo === undefined) {
+        res.redirect("/dashboard");
+        return;
+    }
+
+    const endDate = new Date(date);
+    endDate.setTime(endDate.getTime() + 3600000);
+
+    try {
+        await axios.post("https://www.googleapis.com/calendar/v3/calendars/primary/events", {
+            summary: name,
+            description: description,
+            start: {
+                dateTime: date,
+                timeZone: "Europe/Lisbon"
+            },
+            end: {
+                dateTime: endDate.toISOString(),
+                timeZone: "Europe/Lisbon"
+            }
+        }, {
+            headers: {
+                "Authorization": `Bearer ${req.user.accessToken}`,
+                "Content-Type": "application/json"
+            }
+        });
+
+        res.redirect("/milestones?repo=" + repo + "&success=true");
+    }
+    catch (error) {
+        console.log(error);
+        res.redirect("/milestones?repo=" + repo + "&success=false");
+    }
+});
+
 app.get("/logout", verifyAccess, (_, res) => {
     res.clearCookie("session");
     res.redirect("/");
